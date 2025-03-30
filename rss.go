37c34bd3e3d44d5722163201b90cc8b07e7f95b9
bsuvonov <b.suvonov2022@gmail.com>
2025-03-30 15:26:48 +0800

    unescape posts title and descriptions

--- rss.go
+++ rss.go
@@ -16,7 +16,7 @@ type RSSFeed struct {
 		Title       string    `xml:"title"`
 		Link        string    `xml:"link"`
 		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
+		Items        []RSSItem `xml:"item"`
 	} `xml:"channel"`
 }
 
@@ -54,12 +54,12 @@ func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	xml.Unmarshal(body, &feed)
 
 	p := bluemonday.UGCPolicy()
-    feed.Channel.Description = p.Sanitize(html.UnescapeString(feed.Channel.Description))
-	feed.Channel.Title = p.Sanitize(html.UnescapeString(feed.Channel.Title))
+    feed.Channel.Description = html.UnescapeString(p.Sanitize(feed.Channel.Description))
+	feed.Channel.Title = html.UnescapeString(p.Sanitize(feed.Channel.Title))
 
-	for i := range feed.Channel.Item {
-		feed.Channel.Item[i].Title = p.Sanitize(html.UnescapeString(feed.Channel.Item[i].Title))
-		feed.Channel.Item[i].Description = p.Sanitize(html.UnescapeString(feed.Channel.Item[i].Description))
+	for i := range feed.Channel.Items {
+		feed.Channel.Items[i].Title = html.UnescapeString(p.Sanitize(feed.Channel.Items[i].Title))
+		feed.Channel.Items[i].Description = html.UnescapeString(p.Sanitize(feed.Channel.Items[i].Description))
 	}
 
 	return &feed, nil

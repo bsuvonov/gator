e2fd8ab25f39c66030797f29ccf20b13fa580f7e
bsuvonov <b.suvonov2022@gmail.com>
2025-03-30 15:37:24 +0800

    added queries to posts table

---
+++ sql/queries/posts.sql
@@ -0,0 +1,22 @@
+-- name: CreatePost :one
+INSERT INTO posts(id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8
+)
+RETURNING *;
+
+-- name: GetPostsForUser :many
+WITH userid AS (
+    SELECT id FROM users where users.name = $1
+),
+feed_ids AS (
+    SELECT feed_id FROM feed_follows WHERE user_id = (SELECT id FROM userid)
+)
+SELECT title, url, description, published_at FROM posts WHERE id IN (SELECT f_id FROM feed_ids) ORDER BY updated_at ASC LIMIT $2;
